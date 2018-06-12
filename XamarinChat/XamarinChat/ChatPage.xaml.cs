6251dcac7f370b62d0c554e0a68c2d93eb5bf17a
Soyri <arhisoyri@gmail.com>
2018-06-12 21:43:28 +0300

    Initial

---
+++ XamarinChat/XamarinChat/ChatPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using ScaledroneDotNET;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinChat
+{
+    [JsonObject(MemberSerialization.Fields)]
+    class MemberData
+    {
+        [JsonProperty("name")]
+        public string Name;
+        [JsonProperty("color")]
+        public string Color;
+        [JsonProperty("password")]
+        public string Password;
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ChatPage : ContentPage
+    {
+        private Scaledrone _scaledrone;
+        private const string RoomName = "observable-room";
+
+        public ChatPage (string nickname)
+		{
+            InitializeComponent();
+            var mData = new MemberData { Name = nickname, Color = "green" };
+            _scaledrone = new Scaledrone("ORZLEvNn5v2xcH33", mData);
+            _scaledrone.OnOpened += Scaledrone_OnOpened;
+            _scaledrone.OnRoomMessage += Scaledrone_OnRoomMessage;
+            _scaledrone.Connect();
+
+            //TODO move to send button click
+            _scaledrone.Publish(RoomName, "some fancy message");
+        }
+
+        private void Scaledrone_OnRoomMessage(Room room, Newtonsoft.Json.Linq.JToken message, Member member)
+        {
+        }
+
+        private void Scaledrone_OnOpened()
+        {
+            _scaledrone.Subscribe(RoomName);
+        }
+    }
+}
---
+++ XamarinChat/XamarinChat/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinChat
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnButtonClicked(object sender, EventArgs args)
+        {
+            // await label.RelRotateTo(360, 1000);
+            // await entry.RelRotateTo(360, 1000);
+            var target_page = new ChatPage(entry.Text);
+            
+            await Navigation.PushModalAsync(target_page);
+        }
+    }
+}
